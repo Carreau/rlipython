83937c7541eb353075ec76ce416f0b7011068971
Matthias Bussonnier <bussonniermatthias@gmail.com>
2017-05-30 10:42:58 -0700

    Require Gnureadline conditionnally and run readme through pandoc

--- setup.py
+++ setup.py
@@ -1,10 +1,28 @@
 from distutils.core import setup
 
+with open('readme.md') as f:
+    data = f.read()
+try:
+    import pypandoc
+    description = pypandoc.convert(data, 'rst', format='markdown')
+    print("""
+========================
+pypandoc run sucessfully
+========================""")
+except:
+    print("""
+=================================================================
+pypandoc conversion fail your readme will not be rendered on PyPI
+=================================================================""")
+    description = data
+
+
 setup(
     name='rlipython',
     version='0.1.1',
     packages=['rlipython',],
-    install_requires=["ipython>5.3", "gnureadline"],
+    install_requires=["ipython>5.3"],
+    extras_requires={':sys_platform == "darwin"': ['gnureadline']},
     license='BSD',
     author='The IPython Development Team',
     author_email='ipython-dev@python.org',
